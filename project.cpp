ca7444ea0892c246cf538b1c5d47935641d5b553
TangSaiPang <89617575+TangSaiPang@users.noreply.github.com>
2023-10-19 23:20:46 +0800

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -30,17 +30,45 @@ void Project::show(void)
 
 void Project::new_folder(void)
 {
-    char path_target_temp[1000];
-    char path_not_used_temp[1000] = " \"";
     char path_command[1000] = "xcopy ";
 
+    // 创建目标目录
     mkdir(target_name);
-    getcwd(path_target_temp, 1000);
-    strcat(path_target_temp, "\\");
-    strcat(path_target_temp, target_name);
-    strcat(path_not_used_temp, path_target_temp);  // 在目标路径前添加 "
-    strcat(path_not_used_temp, "\" /e");
+
+    // 复制源目录绝对路径至字符串 path_command
     strcat(path_command, source_name);
-    strcat(path_command, path_not_used_temp);
+    // 字符串修改成 xcopy 源目录 目标目录 /e
+    strcat(path_command, " ");
+    strcat(path_command, get_target());
+    strcat(path_command, " /e");
+    // 执行命令
     system(path_command);
+}
+
+char *Project::path_add_double_quotation_marks(char *Path)
+{
+    // double quotation marks
+    char s[1000] = "\"";
+    strcat(s, Path);
+    strcat(s, "\"");
+    static char rtstr[1000];
+    strcpy(rtstr, s);
+    return rtstr;
+}
+
+char *Project::get_target(void)
+{
+    char path_target[1000];
+    // 获取当前目录绝对路径
+    getcwd(path_target, 1000);
+    // 当前目录绝对路径后加 “\”
+    strcat(path_target, "\\");
+    // 目标目录的绝对路径
+    strcat(path_target, target_name);
+
+    // 目标路径加“ "" ”
+    static char path_last[1000];
+    strcpy(path_last, path_add_double_quotation_marks(path_target));
+    cout << path_last << endl; 
+    return path_last;
 }
